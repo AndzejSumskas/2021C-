a84654fd504d2ae17a5caf546678bfce191f6b5d
DESKTOP-8V7I52D\Andzej <andzejsumskas1@gmail.com>
2021-04-07 22:12:41 +0300

    for ciklas

--- Learning App/Lesson4/Lesson4.cs
+++ Learning App/Lesson4/Lesson4.cs
@@ -66,27 +66,145 @@ namespace Learning_App.Lesson4
 
             //Uzduotis
 
-            Console.WriteLine("Pasirinkite: \n1 Kava \n2 Arbata \n3 Vanduo");
-            int pasirinkimas = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Pasirinkite: \n1 Kava \n2 Arbata \n3 Vanduo");
+            //int pasirinkimas = Convert.ToInt32(Console.ReadLine());
 
-            while (pasirinkimas > 3 || pasirinkimas < 1) 
+            //while (pasirinkimas > 3 || pasirinkimas < 1) 
+            //{
+            //    Console.WriteLine("Klaidinga ivestis");
+            //    Console.WriteLine("Pasirinkite: \n1 Kava \n2 Arbata \n3 Vanduo");
+            //    pasirinkimas = Convert.ToInt32(Console.ReadLine());
+            //}
+            //switch(pasirinkimas)
+            //{
+            //    case 1:
+            //      Console.WriteLine("Kava");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Arbata");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Vanduo");
+            //        break;
+            //}
+
+            //Do while ciklas
+
+            //int input;
+            //do
+            //{
+            //    Console.WriteLine("Pasirinkite: \n1 Kava \n2 Arbata \n3 Vanduo");
+            //    input = Convert.ToInt32(Console.ReadLine());
+            //}
+            //while (input < 1 || input > 3);
+
+            //int vietuPrieLango = 4;
+            //int vietuSkaicius = 8;
+
+            //while (vietuPrieLango > 0 || vietuSkaicius > 0)
+            //{
+            //    Console.WriteLine("Ar norite sedeti prie lango? t/n");
+            //    char pasirinkimas = Convert.ToChar(Console.ReadLine());
+            //    switch (pasirinkimas)
+            //    {
+            //        case 't':
+            //            vietuPrieLango--;
+            //            vietuSkaicius--;
+            //            break;
+            //        case 'n':
+            //            vietuSkaicius--;
+            //            break;
+            //        default:
+            //            Console.WriteLine("Klaida");
+            //            break;
+            //    }
+
+
+
+            //    do
+            //    {
+            //        Console.WriteLine("Ar norite sedeti prie lango? t/n");
+            //        pasirinkimas = Convert.ToChar(Console.ReadLine());
+            //        switch (pasirinkimas)
+            //        {
+            //            case 't':
+            //                vietuPrieLango--;
+            //                vietuSkaicius--;
+            //                break;
+            //            case 'n':
+            //                vietuSkaicius--;
+            //                break;
+            //            default:
+            //                Console.WriteLine("Klaida");
+            //                break;
+            //        }
+            //    } while (vietuPrieLango > 0 && vietuSkaicius > 0);
+
+            //********************************************************
+
+            const int totalseetcount = 8;
+            const int windowseetcount = 4;
+            int allseetstaken = 0;
+            int windowseetstaken = 0;
+            string choise;
+
+           
+
+            while (allseetstaken < totalseetcount || windowseetstaken < windowseetcount)
             {
-                Console.WriteLine("Klaidinga ivestis");
-                Console.WriteLine("Pasirinkite: \n1 Kava \n2 Arbata \n3 Vanduo");
-                pasirinkimas = Convert.ToInt32(Console.ReadLine());
+                do
+                {
+                    Console.WriteLine("Ar norite sedeti prie lango? [yes/no]");
+                    choise = Convert.ToString(Console.ReadLine());
+                } while (choise != "yes" && choise != "no");
+
+                if (choise == "yes")
+                {
+                    windowseetstaken++;   
+                }
+                allseetstaken++;
+
+
             }
-            switch(pasirinkimas)
-            {
-                case 1:
-                  Console.WriteLine("Kava");
-                    break;
-                case 2:
-                    Console.WriteLine("Arbata");
-                    break;
-                case 3:
-                    Console.WriteLine("Vanduo");
-                    break;
-            }
+            //*******************************************
+
+            //int suma = 0;
+            //for (int i = 0; i < 1000; i++)
+            //{
+
+            //    if (i % 5 == 0) 
+            //    {
+            //        continue;
+            //    }               
+            //    if (i > 123)
+            //    {
+            //        break;
+            //    }
+            //    suma += i;
+            //}
+            //Console.WriteLine(suma);
+
+            //int sumaj = 0;
+            //int j = 0;
+
+            //for (;;)
+            //{
+
+            //    if (j % 5 == 0)
+            //    {
+            //        j++;
+            //        continue;
+            //    }
+            //    if (j > 123)
+            //    {
+            //        break;
+            //    }
+            //    sumaj += j;
+            //    j++;
+            //}
+            //Console.WriteLine("j" + sumaj);
         }
     }
 }
+
+
