4ae600f30f706d20fb68d429b113c1084df64842
DESKTOP-8V7I52D\Andzej <andzejsumskas1@gmail.com>
2021-04-07 20:01:23 +0300

    While Ciklas

--- Learning App/Lesson4/Lesson4.cs
+++ Learning App/Lesson4/Lesson4.cs
@@ -35,28 +35,58 @@ namespace Learning_App.Lesson4
             //int sk1 = Convert.ToInt32(Console.ReadLine());
             //int sk2 = Convert.ToInt32(Console.ReadLine());
 
-            //if (sk1 > sk2) { Console.WriteLine("Bad limits!"); }
+            //if (sk1 >= sk2) 
+            //{ 
+            //    Console.WriteLine("Bad limits!"); 
+            //}
             //else
             //{
             //    for (int i = sk1; i < sk2; i++)
             //    {
-            //        Console.WriteLine($"{i} {i*i}");
+            //        Console.WriteLine($"{i} {i * i}");
             //    }
             //}
 
             //Uzduotis 3
-            int skaicius = 0;
 
-            for (int i = 1000; i > 0; i--)
+            //int suma = 0;
+
+            //for (int i = 1000; i > 0; i--)
+            //{
+            //    if (i % 3 == 0 || i % 5 == 0)
+            //    {
+            //        suma += i;
+            //    }             
+            //}
+            //Console.WriteLine("Suma = {0}", suma);
+
+            //************************************************
+
+            //While ciklas
+
+            //Uzduotis
+
+            Console.WriteLine("Pasirinkite: \n1 Kava \n2 Arbata \n3 Vanduo");
+            int pasirinkimas = Convert.ToInt32(Console.ReadLine());
+
+            while (pasirinkimas > 3 || pasirinkimas < 1) 
             {
-                if (i % 3 == 0 || i % 5 == 0)
-                {
-                    skaicius += i;
-
-                }
-                Console.WriteLine("Suma = {0}", skaicius);
+                Console.WriteLine("Klaidinga ivestis");
+                Console.WriteLine("Pasirinkite: \n1 Kava \n2 Arbata \n3 Vanduo");
+                pasirinkimas = Convert.ToInt32(Console.ReadLine());
             }
-
+            switch(pasirinkimas)
+            {
+                case 1:
+                  Console.WriteLine("Kava");
+                    break;
+                case 2:
+                    Console.WriteLine("Arbata");
+                    break;
+                case 3:
+                    Console.WriteLine("Vanduo");
+                    break;
+            }
         }
     }
 }
