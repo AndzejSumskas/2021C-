6fa94d26332bb52cb2f2f0d034ed7c1cbb9e096c
DESKTOP-8V7I52D\Andzej <andzejsumskas1@gmail.com>
2021-04-08 18:06:20 +0300

    Papildoma užduotis - while

--- Learning App/HomeWork4/HomeWork4.cs
+++ Learning App/HomeWork4/HomeWork4.cs
@@ -10,30 +10,57 @@ namespace Learning_App.HomeWork4
     {
         static void Main()
         {
-            int sk1 = 0;
-            int sk2 = 1;
-            int temp;
-            int FSkaiciuKiekis=Convert.ToInt32(Console.ReadLine());
+            //Fibonačio skaičių seka
 
-            if (FSkaiciuKiekis >= 1) 
-            {
-                Console.Write("0 ");
-            }
-            if(FSkaiciuKiekis >= 2)
-            {
-                Console.Write("1 ");
-            }
+            //int sk1 = 0;
+            //int sk2 = 1;
+            //int temp;
+            //int FSkaiciuKiekis=Convert.ToInt32(Console.ReadLine());
 
-            if(FSkaiciuKiekis>2)
+            //if (FSkaiciuKiekis >= 1) 
+            //{
+            //    Console.Write("0 ");
+            //}
+            //if (FSkaiciuKiekis >= 2) 
+            //{
+            //    Console.Write("1 ");
+            //}
+            //if (FSkaiciuKiekis > 2) 
+            //{
+            //    for (int i = 0; i < FSkaiciuKiekis - 2; i++)
+            //    {
+            //        temp = sk1 + sk2;
+            //        sk1 = sk2;
+            //        sk2 = temp;
+            //        Console.Write(temp + " ");
+            //    }
+            //}
+
+            //Papildoma užduotis - while
+
+            int skaicius = new Random().Next(0, 100);           
+            int spejimuSkaicius = 0;                       
+            bool arAtspeta = true;
+            while(arAtspeta)
             {
-                for (int i = 0; i < FSkaiciuKiekis - 2; i++)
+                Console.WriteLine("Atspekite skaiciu nuo 0 iki 100.");
+                int spejimas = Convert.ToInt32(Console.ReadLine());
+                spejimuSkaicius++;
+                if(spejimas>skaicius)
                 {
-                    temp = sk1 + sk2;
-                    sk1 = sk2;
-                    sk2 = temp;
-                    Console.Write(temp + " ");
+                    Console.WriteLine("Mazesnis!");
                 }
-            }     
+                else if(spejimas<skaicius)
+                {
+                    Console.WriteLine("Didesnis!");
+                }
+                else
+                {
+                    Console.WriteLine("Atspejote is {0} karto!", spejimuSkaicius);
+                }
+            }
+
+
         }
     }
 }
